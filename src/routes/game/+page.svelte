c2194d2ee07c93e79567c9efcf55f1bb64449430
MrMancs <marcell.szatmari2007@gmail.com>
2025-11-07 19:10:37 +0100

    Create game-begin feature

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,1 +1,11 @@
-Szia! Ezt csak akkor kéne látnod ha loginoltál :))))
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+	import { Card } from "$lib/components/ui/card";
+</script>
+
+<div class="flex flex-col items-center justify-center h-dvh">
+    <Card class="pt-12 pb-12 max-w-lg w-full flex flex-col justify-center items-center gap-8">
+        <h1 class="text-2xl">Damareen</h1>
+        <Button><a href="/choose-role">Játék indítása</a></Button>
+    </Card>
+</div>

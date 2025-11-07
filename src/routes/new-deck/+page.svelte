35f80128fb08799efe6100e67363eff25cc2d0a4
DeadInside0032 <marktoth341@gmail.com>
2025-11-07 21:39:05 +0100

    created create new deck

---
+++ src/routes/new-deck/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { Card } from "$lib/components/ui/card";
+</script>
+
+<div class="flex flex-col h-dvh">
+
+
+
+    <div class="flex-col flex items-center justify-center pb-12">
+        <h1>Gyűjtemény:</h1>
+        <Card class="h-full pt-12 pb-12 max-w-lg w-full flex flex-col justify-center items-center gap-8">
+            <!--collection cards-->
+        </Card>
+    </div>
+
+        <div class="flex-col flex items-center justify-center pb-12">
+        <h1>Új Pakli:</h1>
+        <Card class="h-full pt-12 pb-12 max-w-lg w-full flex flex-col justify-center items-center gap-8">
+            <!--deck-->
+        </Card>
+    </div>
+
+    <div class="flex flex-col items-center justify-center pt-12">
+        <Button><a href="/player">Tovább</a></Button>
+    </div>
+</div>

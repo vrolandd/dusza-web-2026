71a8d4eb05353946aba19134c866d9c74ebf0de5
MrMancs <marcell.szatmari2007@gmail.com>
2025-11-07 18:41:47 +0100

    Create +layout.svelte
    Create navbar

---
+++ src/routes/game/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+<nav class="h-[50px] bg-accent ">
+    <a href="/" class="text-xl font-bold">Kijelentkezés</a>
+</nav>
+    
+{@render children()}

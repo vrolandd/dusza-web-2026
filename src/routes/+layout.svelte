3cc39bfaeb1a612e0e01ac855f32e32469201311
Vincze Roland <vrolandd.public@gmail.com>
2025-11-07 15:58:05 +0100

    Hello title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,21 +2,21 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
-	import { invalidate } from '$app/navigation'
-  import { onMount } from 'svelte'
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 
-  let { data, children } = $props()
-  let { session, supabase } = $derived(data)
+	let { data, children } = $props();
+	let { session, supabase } = $derived(data);
 
-  onMount(() => {
-    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
-      if (newSession?.expires_at !== session?.expires_at) {
-        invalidate('supabase:auth')
-      }
-    })
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
 
-    return () => data.subscription.unsubscribe()
-  })
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <svelte:head>

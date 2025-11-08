aa374101d050f78f5d5873020fcaeca617923360
MrMancs <marcell.szatmari2007@gmail.com>
2025-11-08 12:09:32 +0100

    Add routing to /game-master

--- src/routes/game-master/+page.svelte
+++ src/routes/game-master/+page.svelte
@@ -9,12 +9,12 @@
     <Card class="pt-12 pb-12 max-w-xl w-full flex flex-col justify-center items-center gap-8">
         <div class="flex flex-row justify-between max-w-xl w-full pl-10 pr-10">
             <div class="flex flex-col gap-5">
-                <Button>Új sima kártya létrehozása</Button>
-                <Button>Új vezér kártya létrehozása</Button>
-                <Button>Új kazamata létrehozása</Button>
+                <Button><a href="/create-basic-card">Új sima kártya létrehozása</a></Button>
+                <Button><a href="/create-leader-card">Új vezér kártya létrehozása</a></Button>
+                <Button><a href="/create-arena">Új kazamata létrehozása</a></Button>
             </div>
             <div class="flex flex-col gap-5 justify-center">
-                <Button>Meglévő kártyák</Button>
+                <Button><a href="/existing-cards">Meglévő kártyák</a></Button>
                 <Button>Játékkörnyezet elnevezése</Button>
             </div>
         </div>

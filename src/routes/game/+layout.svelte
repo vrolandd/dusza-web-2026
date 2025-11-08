ba553b9f66757a84dc588a07ab650b99733f5263
MrMancs <marcell.szatmari2007@gmail.com>
2025-11-08 18:23:47 +0100

    Finish pull

--- src/routes/game/+layout.svelte
+++ src/routes/game/+layout.svelte
@@ -9,12 +9,8 @@
 
 
 <nav class="h-[75px] bg-accent flex items-center justify-end pr-6 sticky top-0">
-<<<<<<< HEAD
-    <Button class="mr-5"><a href="/choose-role">Főmenü</a></Button>
+    <Button class="mr-5"><a href="/game/choose-role">Főmenü</a></Button>
     <Button onclick={signOut}><a href="/auth">Kijelentkezés</a></Button>
-=======
-    <Button onclick={signOut}>Kijelentkezés</Button>
->>>>>>> 1be26591f7ba34259d1ac9b8e2343af8a6f55ed3
 </nav>
 
 <div class="flex justify-center h-[calc(100dvh-75px)] overflow-auto">

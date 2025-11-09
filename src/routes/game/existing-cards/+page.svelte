5752cc6ce94f4b2c64d6b8d63a4d8842a8200e4f
MrMancs <marcell.szatmari2007@gmail.com>
2025-11-09 18:43:16 +0100

    Attempt show images on existing cards

--- src/routes/game/existing-cards/+page.svelte
+++ src/routes/game/existing-cards/+page.svelte
@@ -14,28 +14,33 @@
 
 		return map[element] ?? element;
 	}
+
+	function getImageUrl(path: string) {
+		return `https://ljogpuxophtjfqxllkgs.supabase.co/storage/v1/object/public/cards/${path}`;
+	}
+
 </script>
 
 <h1 class="pt-5 text-center text-4xl">Játékmester - Meglévő</h1>
 <div class="flex flex-col items-center pt-25">
 	<Card class="flex w-full max-w-7xl flex-col items-center justify-center gap-8 pt-12 pb-12">
 		<div class="w-[900px] border border-white flex flex-row justify-center gap-5 pt-5 pb-5 flex-wrap">
-            {#each cards as card}
-                <div class="flex flex-col items-center justify-center">
-					<div class="h-[270px] w-[190px] border border-white ">
-						
+           	{#each cards as card}
+				<div class="flex flex-col items-center justify-center">
+					<div class="h-[270px] w-[190px] border border-white">
+						<img src={getImageUrl(card.image)} alt={card.name} class="h-full w-full object-cover"/>
 					</div>
 					<div class="flex flex-col h-[180px] w-[190px] border border-white pl-5 pr-5">
-						<p class="flex items-center justify-center pt-2 h-[60px]">{card.name}</p>
-						<div class="flex flex-row justify-between items-center h-[60px] pl-12 pr-12">
-							<p>{card.attack}</p>
-							<p>/</p>
-							<p>{card.hp}</p>
-						</div>
-						<p class="flex pb-2 h-[60px] justify-center items-center">{formatElement(card.element)}</p>
+					<p class="flex items-center justify-center pt-2 h-[60px]">{card.name}</p>
+					<div class="flex flex-row justify-between items-center h-[60px] pl-12 pr-12">
+						<p>{card.attack}</p>
+						<p>/</p>
+						<p>{card.hp}</p>
+					</div>
+					<p class="flex pb-2 h-[60px] justify-center items-center">{formatElement(card.element)}</p>
 					</div>	
 				</div>
-            {/each}
+			{/each}
         </div>
 	</Card>
 </div>

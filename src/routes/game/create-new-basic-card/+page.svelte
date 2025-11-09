7420458dc522aa25cf2a6caafd45a71fab338423
MrMancs <marcell.szatmari2007@gmail.com>
2025-11-09 15:09:27 +0100

    Format elements

--- src/routes/game/create-new-basic-card/+page.svelte
+++ src/routes/game/create-new-basic-card/+page.svelte
@@ -8,9 +8,18 @@
 	let cardHealth = "";
 	let cardType = "";
 
-	function createCard() {
-		//TODO Implement card creation logic and instert into database
+	console.log(cardName, cardDamage, cardHealth, cardType);
+
+	function formatElement(e: string) {
+		const map: Record<string, string> = {
+			tuz: "Tűz",
+			viz: "Víz",
+			fold: "Föld",
+			levego: "Levegő"
+		};
+		return map[e] ?? e;
 	}
+
 </script>
 
 <style>
@@ -31,43 +40,44 @@
 	}
 </style>
 
-<h1 class="pt-5 text-center text-4xl">Játékmester - Új sima kártya létrehozása</h1>
-<div class="flex flex-col items-center pt-25">
-	<Card class="flex w-full max-w-xl flex-col items-center justify-center gap-8 pt-12 pb-12">
-		<div class="flex w-full max-w-xl flex-row justify-between pr-10 pl-10">
-			<div class="flex flex-col items-center justify-center">
-				<div class="h-[270px] w-[190px] border border-white ">
-					
-				</div>
-				<div class="flex flex-col h-[180px] w-[190px] border border-white pl-5 pr-5">
-					<p class="flex items-center justify-center pt-2 h-[60px]">{cardName}</p>
-					<div class="flex flex-row justify-between items-center h-[60px] pl-12 pr-12">
-						<p>{cardDamage}</p>
-						<p>/</p>
-						<p>{cardHealth}</p>
+<form method="POST">
+
+	<h1 class="pt-5 text-center text-4xl">Játékmester - Új sima kártya létrehozása</h1>
+	<div class="flex flex-col items-center pt-25">
+		<Card class="flex w-full max-w-xl flex-col items-center justify-center gap-8 pt-12 pb-12">
+			<div class="flex w-full max-w-xl flex-row justify-between pr-10 pl-10">
+				<div class="flex flex-col items-center justify-center">
+					<div class="h-[270px] w-[190px] border border-white ">
+						
 					</div>
-					<p class="flex pb-2 h-[60px] justify-center items-center">{cardType}</p>
-				</div>	
-			</div>
-			<div class="flex flex-col items-center justify-center gap-5">
-				<label for="">Kártya neve</label>
-				<Input type="text" placeholder="Aragorn" bind:value={cardName} />
-				<label for="">Kártya sebzése</label>
-				<Input type="number" min="1" max="100" bind:value={cardDamage} placeholder="6" />
-				<label for="">Kártya életereje</label>
-				<Input type="number" min="2" max="100" bind:value={cardHealth} placeholder="10" />
-				<label for="">Kártya típusa</label>
-				<select name="" id="" class="border border-white bg-black w-full p-2 rounded-[4px] text-center" bind:value={cardType}>
-					<option value="Tűz">Tűz</option>
-					<option value="Víz">Víz</option>
-					<option value="Föld">Föld</option>
-					<option value="Levegő">Levegő</option>
-				</select>
-				<div role="button" tabindex="0" onclick={createCard}>
-					<Button class="mt-5 rounded-[4px] cursor-pointer w-full p-2" >Kártya létrehozása</Button>
+					<div class="flex flex-col h-[180px] w-[190px] border border-white pl-5 pr-5">
+						<p class="flex items-center justify-center pt-2 h-[60px]">{cardName}</p>
+						<div class="flex flex-row justify-between items-center h-[60px] pl-12 pr-12">
+							<p>{cardDamage}</p>
+							<p>/</p>
+							<p>{cardHealth}</p>
+						</div>
+						<p class="flex pb-2 h-[60px] justify-center items-center">{formatElement(cardType)}</p>
+					</div>	
 				</div>
-				
+				<div class="flex flex-col items-center justify-center gap-5">
+					<label for="">Kártya neve</label>
+					<Input type="text" name="title" placeholder="Aragorn" bind:value={cardName} />
+					<label for="">Kártya sebzése</label>
+					<Input type="number" name="damage" min="1" max="100" bind:value={cardDamage} placeholder="6" />
+					<label for="">Kártya életereje</label>
+					<Input type="number" name="hp" min="2" max="100" bind:value={cardHealth} placeholder="10" />
+					<label for="">Kártya típusa</label>
+					<select name="type" id="" class="border border-white bg-black w-full p-2 rounded-[4px] text-center" bind:value={cardType}>
+						<option value="tuz">Tűz</option>
+						<option value="viz">Víz</option>
+						<option value="fold">Föld</option>
+						<option value="levego">Levegő</option>
+					</select>
+					<Button class="mt-5 rounded-[4px] cursor-pointer w-full p-2" type="submit">Kártya létrehozása</Button>
+				</div>
 			</div>
-		</div>
-	</Card>
-</div>
+		</Card>
+	</div>
+
+</form>
--- src/routes/game/existing-cards/+page.svelte
+++ src/routes/game/existing-cards/+page.svelte
@@ -1,12 +1,41 @@
 <script lang="ts">
 	import { Card } from '$lib/components/ui/card';
+	export let data
+
+    let cards = data.cards
+
+	function formatElement(element: string): string {
+		const map: Record<string, string> = {
+			tuz: "Tűz",
+			viz: "Víz",
+			fold: "Föld",
+			levego: "Levegő"
+		};
+
+		return map[element] ?? element;
+	}
 </script>
 
 <h1 class="pt-5 text-center text-4xl">Játékmester - Meglévő</h1>
 <div class="flex flex-col items-center pt-25">
 	<Card class="flex w-full max-w-7xl flex-col items-center justify-center gap-8 pt-12 pb-12">
 		<div class="w-[900px] border border-white flex flex-row justify-center gap-5 pt-5 pb-5 flex-wrap">
-            <!--existing cards from database-->
+            {#each cards as card}
+                <div class="flex flex-col items-center justify-center">
+					<div class="h-[270px] w-[190px] border border-white ">
+						
+					</div>
+					<div class="flex flex-col h-[180px] w-[190px] border border-white pl-5 pr-5">
+						<p class="flex items-center justify-center pt-2 h-[60px]">{card.name}</p>
+						<div class="flex flex-row justify-between items-center h-[60px] pl-12 pr-12">
+							<p>{card.attack}</p>
+							<p>/</p>
+							<p>{card.hp}</p>
+						</div>
+						<p class="flex pb-2 h-[60px] justify-center items-center">{formatElement(card.element)}</p>
+					</div>	
+				</div>
+            {/each}
         </div>
 	</Card>
 </div>
